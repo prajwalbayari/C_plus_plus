b11318ffbdf891a0341a3b78922dd8bcaf5e7b65
prajwalbayari <prajwalbayari4@gmail.com>
2025-03-01 19:23:32 +0530

    Modified CycleDetectionDG and added Safe nodes

--- Striver/Graph/SafeNodes.cpp
+++ Striver/Graph/SafeNodes.cpp
@@ -1,24 +1,21 @@
-//Cycle detection in an Directed graph
+//Given a directed graph find the list of nodes that will eventually enter the safe mode
+//A node is safe if it reaches a terminal node
 
 #include<iostream>
 #include<vector>
 #include<queue>
 using namespace std;
 
-//Using DFS
-
 bool DFS(vector<vector<int>>& edges,vector<int>& vis,int src,vector<int>& path){
-    vis[src]=1;
-    path[src]=1;
+    vis[src]=1,path[src]=1;
     for(auto node:edges[src]){
         if(!vis[node]){
             if(DFS(edges,vis,node,path))
                 return true;
-        }
-        else if(path[node])
+        }else if(path[node])
             return true;
     }
-    path[src]=0;
+    path[src]=0,vis[src]=2;
     return false;
 }
 
@@ -26,24 +23,29 @@ int main(){
     int m,n;
     cout<<"Enter the number of nodes and edges: ";
     cin>>n>>m;
+    cout<<"Enter the edges:\n";
     vector<vector<int>> edges(n+1);
-    cout<<"Enter the edges: ";
     for(int i=0;i<m;i++){
         int u,v;
         cin>>u>>v;
         edges[u].push_back(v);
     }
+
     vector<int> vis(n+1,0),path(n+1,0);
     for(int i=1;i<=n;i++){
-        if(!vis[i] && DFS(edges,vis,i,path)){
-            cout<<"There is a cycle present\n";
-            return 0;
-        }
+        if(vis[i]==0)
+            DFS(edges,vis,i,path);
     }
 
-    cout<<"There is no cycle present\n";
-    return 0;
+    cout<<"The safe nodes are: ";
+
+    for(int i=1;i<=n;i++){
+        if(vis[i]==2)
+            cout<<i<<" ";
+    }
+    cout<<endl;
 }
 
-//Time complexity: O(n+m)
+
 //Space complexity: O(n)
+//Time complexity: O(n)
