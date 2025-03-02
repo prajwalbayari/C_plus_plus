88cfb05f0e91bbaaffb7354c965b2984e739d600
prajwalbayari <prajwalbayari4@gmail.com>
2025-03-02 14:48:59 +0530

    Eventual safe nodes using topological sort

--- Striver/Graph/SafeNodes.cpp
+++ Striver/Graph/SafeNodes.cpp
@@ -4,7 +4,10 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
 using namespace std;
+
+//Using DFS
 
 bool DFS(vector<vector<int>>& edges,vector<int>& vis,int src,vector<int>& path){
     vis[src]=1,path[src]=1;
@@ -19,6 +22,34 @@ bool DFS(vector<vector<int>>& edges,vector<int>& vis,int src,vector<int>& path){
     return false;
 }
 
+//Using BFS
+//1)Reverse all the edges and perform Toposort
+
+void BFS(vector<vector<int>>& edges,vector<int>& ans){
+    int n=edges.size();
+    vector<int> indeg(n,0);
+    for(int i=0;i<n;i++){
+        for(int node:edges[i])
+            indeg[node]++;
+    }
+    queue<int> q;
+    for(int i=1;i<n;i++){
+        if(indeg[i]==0)
+            q.push(i);
+    }
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        ans.push_back(node);
+        for(int child:edges[node]){
+            indeg[child]--;
+            if(indeg[child]==0)
+                q.push(child);
+        }
+    }
+}
+
 int main(){
     int m,n;
     cout<<"Enter the number of nodes and edges: ";
@@ -28,24 +59,41 @@ int main(){
     for(int i=0;i<m;i++){
         int u,v;
         cin>>u>>v;
-        edges[u].push_back(v);
+        // edges[u].push_back(v); //For DFS
+        edges[v].push_back(u); //For BFS reversing the nodes
     }
 
-    vector<int> vis(n+1,0),path(n+1,0);
-    for(int i=1;i<=n;i++){
-        if(vis[i]==0)
-            DFS(edges,vis,i,path);
-    }
+    //For DFS
 
-    cout<<"The safe nodes are: ";
+    // vector<int> vis(n+1,0),path(n+1,0);
+    // for(int i=1;i<=n;i++){
+    //     if(vis[i]==0)
+    //         DFS(edges,vis,i,path);
+    // }
 
-    for(int i=1;i<=n;i++){
-        if(vis[i]==2)
-            cout<<i<<" ";
-    }
+    // cout<<"The safe nodes are: ";
+
+    // for(int i=1;i<=n;i++){
+    //     if(vis[i]==2)
+    //         cout<<i<<" ";
+    // }
+    // cout<<endl;
+
+    //For BFS
+
+    vector<int> ans;
+    BFS(edges,ans);
+    sort(ans.begin(),ans.end());
+    cout<<"The eventual safe nodes are: ";
+    for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
     cout<<endl;
 }
 
-
+//DFS
 //Space complexity: O(n)
 //Time complexity: O(n)
+
+//BFS
+//Space complexity: O(n*m) for storing the reversed edges
+//Time complexity : O(n+m+n(logn)) For sorting and Topological sort
